d93d06bb6701b07c1e47130b3321405bb07f859d
SVR Kalluru <raghuk.vit@gmail.com>
2024-04-22 06:07:37 +0530

    Working on CI

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "ami" {
-  ami           = data.aws_ami.ami.image_id
-  instance_type = "t3.small"
+  ami                    = data.aws_ami.ami.image_id
+  instance_type          = "t3.small"
   vpc_security_group_ids = [data.aws_security_group.allow-all.id]
 
   tags = {
